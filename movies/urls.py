8b3803d120e8a14acc1163ef9ec749c1c3229522
Matthew-Ballmer <vincent.roth@yandex.ru>
2017-07-17 00:44:08 +0300

    Add login and logout pages

--- movies/urls.py
+++ movies/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,4 +7,6 @@ app_name = 'movies'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^update/$', views.update, name='update'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- omnium_gatherum/settings.py
+++ omnium_gatherum/settings.py
@@ -106,6 +106,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Authentication
+
+LOGIN_REDIRECT_URL = '/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
