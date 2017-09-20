9042cf9a531b97a502bdbc867c410b2bbf0cc9a0
vvnc <vincent.roth@yandex.ru>
2017-09-20 19:59:34 +0300

    Implement released movies tab
    Show movies with already released DVDs

--- movies/urls.py
+++ movies/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'movies'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.get_all_movies, name='index'),
     url(r'^update/$', views.update, name='update'),
     url(r'^add-movies/$', views.add_movies, name='add-movies'),
+    url(r'^released/$', views.get_released_movies, name='get-released'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,21 +1,43 @@
+import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 
-from django.conf import settings
-
 from .models import TmdbMovie
 
 
-def index(request):
+class MovieType( object ):
+    ALL = 'all'
+    RELEASED = 'released'
+    NOT_RELEASED = 'not-released'
+    UNKNOWN = 'unknown'
+
+
+def get_all_movies(request):
     dvd_releases = TmdbMovie.objects.all().order_by('-us_physical_release_date')
 
     context = {
         'dvd_releases': dvd_releases,
+        'movies_type': MovieType.ALL,
     }
     return render(request, 'movies/index.html', context)
 
+
+def get_released_movies(request):
+    dvd_releases = TmdbMovie.objects.all().filter(
+        us_physical_release_date__isnull=False
+    ).filter (
+        us_physical_release_date__lt=datetime.datetime.today()
+    ).order_by(
+        '-us_physical_release_date'
+    )
+    context = {
+        'dvd_releases': dvd_releases,
+        'movies_type': MovieType.RELEASED,
+    }
+    return render(request, 'movies/index.html', context)
 
 @staff_member_required
 def update(request):
